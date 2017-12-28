ac925bfd011763958076e814c9cac71a08231dc3
Mark Movida <mrkmvd@gmail.com>
2017-12-28 12:06:11 -0800

    webhook endpoints

--- src/app.js
+++ src/app.js
@@ -8,12 +8,63 @@ const app = express();
 
 var path  = require("path");
 
-
 app.get('/', (req, res) => {
   // res.status(200).send('Agent21 tickets').end();
-    res.sendFile(path.join(__dirname+'/index.html'));
+    res.sendFile(path.join(__dirname+'/index.html'));  
+});
 
-  
+
+app.post('/api/actions', (req, res) => {
+	console.log("Called /api/actions");
+	
+	res.setHeader('Content-Type', 'application/json'); //Requires application/json MIME type
+
+	var resData = {};
+	resData.speech = '';
+	resData.displayText = 'Thanks for the feedback! /api/actions..';
+	resData.data = {};
+	resData.contextOut = [];
+	resData.source = "master";
+	resData.followupEvent = {};
+	res.status(200).json(resData);
+});
+
+app.post('/api/feedback/positive', (req, res) => {
+	var resData = {};
+	resData.speech = 'positive_feedback';
+	resData.displayText = 'Thanks for the feedback';
+	resData.data = {};
+	resData.contextOut = [];
+	resData.source = "master";
+	resData.followupEvent = {};
+	res.status(200).json(resData);
+});
+
+app.post('/api/feedback/negative', (req, res) => {
+	var resData = {};
+	resData.speech = 'negative_feedback';
+	resData.displayText = 'Thanks for the feedback';
+	resData.data = {};
+	resData.contextOut = [];
+	resData.source = "master";
+	resData.followupEvent = {};
+	res.status(200).json(resData);
+});
+
+app.post('/api/conversation', (req, res) => {
+	var resData = {};
+	resData.speech = 'conversation';
+	resData.displayText = 'Hello. How may I assist you?';
+	resData.data = {};
+	resData.contextOut = [];
+	resData.source = "master";
+	resData.followupEvent = {};
+	res.status(200).json(resData);	
+});
+
+app.post('/api', (req, res) => {
+	var resData = {};
+	res.status(200).json(resData);	
 });
 
 // Start the server
